5d2d3ec4b39dac0308c8e90185e1145aa147a4e7
Venc03 <vegi.dani1@gmail.com>
2024-02-12 13:36:48 +0100

    2. feladat

--- src/Fields/Tabla.java
+++ src/Fields/Tabla.java
@@ -1,4 +1,44 @@
 package Fields;
 public class Tabla {
     
+    
+    
+    private int T[][];
+    private boolean UresCella;
+    
+    
+    
+    public void Elhelyez(){
+        
+    }
+    
+    
+    
+    public void Fájlbair(){
+        
+    }
+    
+    
+    
+    public void Megjelenit(){
+        
+    }
+    
+    
+    
+    public void Tabla(){
+        
+    }
+    
+    
+    
+    public void UresOszlop(){
+        
+    }
+    
+    
+    
+    public void UresSor(){
+        
+    }
 }
