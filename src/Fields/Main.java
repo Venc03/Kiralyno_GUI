f6b4f25c907f00dea9e61fd8e622a23faeb5574e
Venc03 <vegi.dani1@gmail.com>
2024-02-12 14:37:08 +0100

    3. feladat fejlesztese, 4. feladat

--- src/Fields/Main.java
+++ src/Fields/Main.java
@@ -1,7 +1,12 @@
 package Fields;
+
 public class Main {
     public static void main(String[] args) {
-        
+        run();
     }
     
+    public static void run(){
+        boolean[][] myTable = new boolean[8][8]; // Example: 8x8 table
+        Tabla t = new Tabla(myTable, true);
+    }  
 }
--- src/Fields/Tabla.java
+++ src/Fields/Tabla.java
@@ -3,48 +3,51 @@ package Fields;
 public class Tabla {
 
     private boolean T[][];
-    private boolean UresCella = true;
+    private boolean UresCella;
 
-
-    
     public Tabla(boolean[][] T, boolean UresCella) {
-        this.T = T; 
+        this.T = T;
         this.UresCella = UresCella;
-    }
-    
 
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                T[i][j] = UresCella;
+                if (T[i][j]) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); 
+        }
+    }
 
     public void Elhelyez() {
-        
+
     }
-    
-    
-    
+
     public void Fajlbair() {
-        
+
     }
-    
-    
-    
+
     public void Megjelenit() {
-        
-    }
-
-    
-    
-    public void UresOszlop() {
-        for (int j = 0; j < T.length; j++) { 
-            for (int i = 0; i < T[j].length; i++) { 
-                T[i][j] = UresCella;
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                if (T[i][j]) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
             }
+            System.out.println(); 
         }
     }
 
-    
-    
-    public void UresSor(int rowI) {
-        for (int j = 0; j < T[rowI].length; j++) {
-            T[rowI][j] = UresCella;
-        }
+    public void UresOszlop() {
+
+    }
+
+    public void UresSor() {
+
     }
 }
