61e928bcfdcd5fb33d4ca8588701d1d51219f232
Venc03 <vegi.dani1@gmail.com>
2024-02-12 14:04:42 +0100

    3. feladat

--- src/Fields/Tabla.java
+++ src/Fields/Tabla.java
@@ -1,44 +1,50 @@
 package Fields;
+
 public class Tabla {
+
+    private boolean T[][];
+    private boolean UresCella = true;
+
+
     
+    public Tabla(boolean[][] T, boolean UresCella) {
+        this.T = T; 
+        this.UresCella = UresCella;
+    }
     
-    
-    private int T[][];
-    private boolean UresCella;
-    
-    
-    
-    public void Elhelyez(){
+
+
+    public void Elhelyez() {
         
     }
     
     
     
-    public void Fájlbair(){
+    public void Fajlbair() {
         
     }
     
     
     
-    public void Megjelenit(){
+    public void Megjelenit() {
         
     }
+
     
     
-    
-    public void Tabla(){
-        
+    public void UresOszlop() {
+        for (int j = 0; j < T.length; j++) { 
+            for (int i = 0; i < T[j].length; i++) { 
+                T[i][j] = UresCella;
+            }
+        }
     }
+
     
     
-    
-    public void UresOszlop(){
-        
-    }
-    
-    
-    
-    public void UresSor(){
-        
+    public void UresSor(int rowI) {
+        for (int j = 0; j < T[rowI].length; j++) {
+            T[rowI][j] = UresCella;
+        }
     }
 }
