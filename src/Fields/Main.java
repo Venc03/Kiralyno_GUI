6c91e539f7dd6077628a17058dfd1dbf150534b2
Venc03 <vegi.dani1@gmail.com>
2024-02-12 15:11:48 +0100

    5. feladat

--- src/Fields/Main.java
+++ src/Fields/Main.java
@@ -4,9 +4,17 @@ public class Main {
     public static void main(String[] args) {
         run();
     }
-    
-    public static void run(){
-        boolean[][] myTable = new boolean[8][8]; // Example: 8x8 table
-        Tabla t = new Tabla(myTable, true);
-    }  
+
+    public static void run() {
+        boolean[][] table = new boolean[8][8];
+        Tabla t = new Tabla(table, true);
+        t.Megjelenit();
+        t.Elhelyez(3);
+        t.Megjelenit();
+        int emptyColumn = t.UresOszlop();
+        int emptyRow = t.UresSor();
+        System.out.println("Empty Column: " + emptyColumn);
+        System.out.println("Empty Row: " + emptyRow);
+        t.Fajlbair(); // You can call this method to write the current state to a file
+    }
 }
--- src/Fields/Tabla.java
+++ src/Fields/Tabla.java
@@ -1,53 +1,76 @@
 package Fields;
 
-public class Tabla {
+import java.util.Random;
 
+public class Tabla {
     private boolean T[][];
     private boolean UresCella;
 
     public Tabla(boolean[][] T, boolean UresCella) {
         this.T = T;
         this.UresCella = UresCella;
+    }
 
-        for (int i = 0; i < T.length; i++) {
-            for (int j = 0; j < T[i].length; j++) {
-                T[i][j] = UresCella;
-                if (T[i][j]) {
-                    System.out.print("#");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println(); 
+    public void Elhelyez(int N) {
+        Random r = new Random();
+        int numRows = T.length;
+        int numCols = T[0].length;
+
+        N = Math.min(N, numRows * numCols);
+
+        for (int count = 0; count < N; count++) {
+            int row, col;
+            do {
+                row = r.nextInt(numRows);
+                col = r.nextInt(numCols);
+            } while (T[row][col]);
+
+            T[row][col] = true;
         }
     }
 
-    public void Elhelyez() {
-
-    }
-
     public void Fajlbair() {
-
+        // Implement file writing logic here
     }
 
     public void Megjelenit() {
         for (int i = 0; i < T.length; i++) {
             for (int j = 0; j < T[i].length; j++) {
                 if (T[i][j]) {
-                    System.out.print("#");
+                    System.out.print("K ");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("# ");
                 }
             }
-            System.out.println(); 
+            System.out.println();
         }
     }
 
-    public void UresOszlop() {
-
+    public int UresOszlop() {
+        for (int j = 0; j < T[0].length; j++) {
+            boolean isEmpty = true;
+            for (int i = 0; i < T.length; i++) {
+                if (T[i][j]) {
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if (isEmpty) return j;
+        }
+        return -1; // No empty column found
     }
 
-    public void UresSor() {
-
+    public int UresSor() {
+        for (int i = 0; i < T.length; i++) {
+            boolean isEmpty = true;
+            for (int j = 0; j < T[i].length; j++) {
+                if (T[i][j]) {
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if (isEmpty) return i;
+        }
+        return -1; // No empty row found
     }
 }
